d4890625c331b6db72543b8096956d8802300c78
Lucas Almeida <72702201+zzzluquinhas@users.noreply.github.com>
2024-04-21 23:20:31 -0300

    removed dead code

--- back/classes.py
+++ back/classes.py
@@ -1,35 +1,12 @@
 #Arquivo para consulta do nome dos campos (manter os nomes dos atributos iguais aos usados no firebase)
 
 class User:
-    def __init__(self, id, login, password, pokemon=[], teams=[]):
-        self.id = id
+    def __init__(self, login, password, pokemonList=[]):
         self.login = login
         self.password = password
-        self.pokemon = pokemon
-        self.teams = teams
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'login': self.login,
-            'password': self.password,
-            'pokemons': [pokemon.to_dict() for pokemon in self.pokemon],
-            'teams': [team.to_dict() for team in self.teams]
-        }
+        self.pokemonList = pokemonList
 
 class Pokemon:
-    def __init__(self, id, name, nickname, pokedex, user=None):
-        self.id = id
-        self.name = name
+    def __init__(self, pokemonID, nickname):
+        self.pokemonID = pokemonID
         self.nickname = nickname
-        self.pokedex = pokedex
-        self.user = user
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'nickname': self.nickname,
-            'pokedex': self.pokedex,
-            'user': self.user.to_dict() if self.user else None
-        }
